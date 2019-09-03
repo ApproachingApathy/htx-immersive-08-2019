1f771b454976f84f4f48d52ec07805a377973c43
Ephriam Henderson <orik162@gmail.com>
2019-09-03 16:08:10 -0500

    Added Functionality to phonebook.

--- 02-week/1-tuesday/labs/henderson-ephriam/phone-book.py
+++ 02-week/1-tuesday/labs/henderson-ephriam/phone-book.py
@@ -41,7 +41,41 @@ while True:
             print("No entries found.")
 
         print("Search Complete")
+    elif option == 2:
+        new_contact = input("Input a name: ")
+        new_number = input("Input a number (555-555-5555): ")
 
+        answer = input(f"Is {new_contact}: {new_number} correct? Y/N ").lower()
+        while True:
+            if answer == "y":
+                phonebook_dict[new_contact] = new_number
+                print("Contact Saved.")
+                break
+            elif answer == "n":
+                print("Contact not saved")
+            else:
+                answer = input("Input Y/N ")
+    elif option == 3:
+        marked_contact = input("Enter a name for deletion: ").lower()
+        is_entry_found = False
+        for key in phonebook_dict:
+            if key.lower() == marked_contact:
+                is_entry_found = True
+                answer = input(f"Would you like to delete {key}? Y/N ").lower()
+                if answer == "y":
+                    del phonebook_dict[key]
+                    print("Contact Deleted")
+                elif answer == "n":
+                    print("Contact not Deleted.")
+                else:
+                    answer = input("Input Y/N ")
+            if is_entry_found == False:
+                print("No entries found.")
+    elif option == 4:
+        for key in phonebook_dict:
+            print(f"{key}: {phonebook_dict[key]}")
+    elif option == 5:
+        exit(0)
     else:
         print("Input a valid command")
 
