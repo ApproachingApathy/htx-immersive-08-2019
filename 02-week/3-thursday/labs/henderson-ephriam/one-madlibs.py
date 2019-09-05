27a7859514576f220b63adf77a030b5fb5aeef90
Ephriam Henderson <orik162@gmail.com>
2019-09-05 13:32:49 -0500

    Added one-madlibs.

---
+++ 02-week/3-thursday/labs/henderson-ephriam/one-madlibs.py
@@ -0,0 +1,7 @@
+def madlib(name, subject):
+    print(f"{name}'s favorite subject is {subject}.")
+
+name = input("Input a name. ")
+subject = input("Input a academic subject. ")
+
+madlib(name, subject)
