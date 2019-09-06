5d5bcd547b8046390d88ff31663e7ed628443ed2
Ephriam Henderson <orik162@gmail.com>
2019-09-06 09:02:48 -0500

    Modified Tic-Tac-Toe

--- 02-week/3-thursday/labs/henderson-ephriam/12-tic-tac-toe.py
+++ 02-week/3-thursday/labs/henderson-ephriam/12-tic-tac-toe.py
@@ -1,6 +1,24 @@
 import time
 import os
 
+def clr_screen():
+    os.system("cls||clear")
+
+def turn_changer(player):
+    global turn
+    if player == "X":
+        turn = "O"
+    else:
+        turn =  "X"
+
+def clr_board():
+    board = [
+        [" "," "," "],
+        [" "," "," "],
+        [" "," "," "]
+    ]
+
+    return board
 def drw_intro():
     os.system("cls||clear")
     print("=====================================")
@@ -9,12 +27,8 @@ def drw_intro():
     time.sleep(3.5)
     #os.system("cls||clear")
 
-def drw_board():
-    board = [
-        [" "," "," "],
-        [" "," "," "],
-        [" "," "," "]
-        ]
+def drw_board(board):
+    
     print("  | 1 | 2 | 3 |")
     print("---------------")
     print(f"1 | {board[0][0]} | {board[0][1]} | {board[0][2]} |")
@@ -24,8 +38,17 @@ def drw_board():
     print(f"3 | {board[2][0]} | {board[2][1]} | {board[2][2]} |")
     print("---------------")
 
-#def move(board, location, player):
+def move(board, location, player):
     #TODO
+    if board[0][location[0]][location[1]] == " ":
+        board[location[0]][location[1]] = player
+        turn_changer(player)
+        return [board, True]
+    else:
+        print("That position is already filled!")
+        return [board[0], False]
+
+
 
 
 def get_move():
@@ -48,13 +71,24 @@ def get_move():
                 print("I couldn't understand your intentions. Try again.")
                 time.sleep(1.5)
                 break
-        
-        
+    return positions
 
-
-
-
-
+board = [clr_board(), False]
+turn = "X"
 drw_intro()
-drw_board()
-get_move()
+
+while True:
+    print(f"It's {turn}'s turn.'")
+    drw_board(board[0])
+    location = get_move()
+    turn_success = False
+    while turn_success == False:
+        print("DEBUG")
+        print(location)
+        board = move(board, location, turn)
+        if board != 0 :
+            turn_success = True
+        else:
+            location = get_move()
+
+    clr_screen()
