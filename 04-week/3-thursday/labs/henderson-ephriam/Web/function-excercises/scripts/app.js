873c1e0909db05a289ff2254ca07e94fa712a03c
Ephriam Henderson <orik162@gmail.com>
2019-09-20 07:44:04 -0500

    Thursday Week 3

---
+++ 04-week/3-thursday/labs/henderson-ephriam/Web/function-excercises/scripts/app.js
@@ -0,0 +1,58 @@
+function getPositiveNumbers(arrNumbers) {
+    let result = arrNumbers.filter(number => number > 0);
+    return result;
+}
+
+function getEvenNumbers(arrNumbers) {
+    let result =  arrNumbers.filter(function(number) {
+        let isDivisibleBy2 = (number % 2) == 0;
+        if (number != 0 && isDivisibleBy2) {
+            return true;
+        }
+        return false;
+    })
+    return result;
+}
+
+function squareNumbers(arrNumbers) {
+    let result = arrNumbers.map(number => number**2)
+    return result;
+}
+
+function getHotCities(arrCities) {
+    let result = arrCities.filter(objCity => objCity.temperature > 70)
+    return result;
+}
+
+function getCityNames(arrCities) {
+    let result = arrCities.map(objCity => objCity.name)
+    return result;
+}
+
+// function goodJobInator(arrName) {
+//     let re
+// }
+
+function sortAlpha(arrStr) {
+    return arrStr.sort()
+}
+
+var numbers = [-276, 5, 7,32, -12, 15, 17, 992, 137, 11, 42, 12, 0, -4]
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 } ];
+
+var people = [ 'Dom', 'Lyn', 'Kirk', 'Autumn', 'Trista', 'Jesslyn', 'Kevin', 'John', 'Eli', 'Juan', 'Robert', 'Keyur', 'Jason', 'Che', 'Ben' ];
+
+console.log(getPositiveNumbers(numbers))
+console.log(getEvenNumbers(numbers))
+console.log(squareNumbers(numbers))
+console.log(getHotCities(cities))
+console.log(getCityNames(cities))
+console.log(sortAlpha(people))
+
+
+
+
